64f3a89b2ec12921fb8a29570976b88c73d28f71
koichiro furusho <uranus0824@gmail.com>
2024-02-06 23:57:50 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,35 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+$(function(){
+    $('#top-title').hide().fadeIn(2500);
+});
+
+$(function(){
+// アニメーションさせたいクラス
+var container = $(".demoTxt");
+// アニメーションスピード
+var speed = 80;
+
+// テキストの間にスペースを入れます
+var content = container.html();
+var text = $.trim(content);
+var newHtml = "";
+
+// スペースで区切ったテキストを、テキストの数だけspanで囲む
+text.split("").forEach(function(v) {
+ newHtml += '<span>' + v + '</span>';
+});
+
+// spanで囲んだテキスト群をHTMLに戻す
+container.html(newHtml);
+
+// 1文字ずつ表示
+var txtNum = 0;
+container.css({opacity: 1});
+setInterval(function() {
+  container.find('span').eq(txtNum).css({opacity: 1});
+ txtNum++
+}, speed);
+});
